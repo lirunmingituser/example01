75fe3d5dd3c4f1a5726909f177092f7af4fba173
Li <email>
2019-04-09 09:05:25 -0400

    updatefrom Monday

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 9000;
-//app.get('/:text', (req, res) => res.send(req.params.text));
-app.get('/', (req, res) => res.send('Hello World!!'));
-app.listen(port, () => console.log(`Sample App ${port}!`));
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+	if (req.cookies.vcount == undefined) {
+		req.vcount = 1;
+		res.cookie("vcount", 1);
+	} else {
+		req.vcount = parseInt(req.cookies.vcount) + 1;
+		res.cookie("vcount", req.vcount);
+	}
+	next();
+});
+
+app.get("/inc", (req, res) => {
+	return res.send(path.join(__dirname, "inc"));
+});
+app.use("/inc", express.static(path.join(__dirname, "inc")));
+
+app.get("/hello", (req, res) => {
+	return res.send(req.vcount + "<br>hey there");
+});
+
+app.get("/users/:userId/:email", (req, res) => {
+	let uid = parseInt(req.params.userId);
+	let email = req.params.email;
+
+	res.send(`user ${uid}'s email set to ${email}`);
+});
+
+app.get("/numbers/:from-:to", (req, res) => {
+	let a = parseInt(req.params.from);
+	let b = parseInt(req.params.to);
+
+	let html = "";
+
+	if (a > b) {
+		let t = a;
+		a = b;
+		b = t;
+	}
+
+	for (let x = a; x <= b; x++) {
+		html += `<div>${x}</div>`;
+	}
+
+	res.send(html);
+});
+
+app.listen(port, function() {
+	return console.log("Sample App " + port + "!");
+});
